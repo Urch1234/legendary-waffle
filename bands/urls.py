1331f7c3d63286fd6c1f91e08813ccd8df9261de
Ihezie Ernest <iernest61@gmail.com>
2024-09-18 18:02:53 +0100

    Updated view and musicians template

--- bands/urls.py
+++ bands/urls.py
@@ -4,4 +4,5 @@ from bands import views
 
 urlpatterns = [
     path("musician/<int:musician_id>/", views.musician_detail, name="musician"),
+    path("musicians/", views.musicians, name="musicians"),
 ]
